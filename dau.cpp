3d9b23661064f8df0aa0f0ec968dbd8ed715c4b7
ntm21206 <156528351+ntm21206@users.noreply.github.com>
2025-09-11 01:42:31 +0700

    Update dau.cpp

--- dau.cpp
+++ dau.cpp
@@ -1,7 +1,9 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <windows.h>
 #include <conio.h>
+using namespace std;
 
+// ==== Hàm ti?n ích (thay cho mylib.h) ====
 void gotoXY(int x, int y) {
     HANDLE hConsoleOutput;
     COORD Cursor_an_Pos = { (SHORT)x, (SHORT)y };
@@ -9,87 +11,58 @@ void gotoXY(int x, int y) {
     SetConsoleCursorPosition(hConsoleOutput, Cursor_an_Pos);
 }
 
-using namespace std;
-int main()
-{
-  int i=1;
-  int x = 10, y = 0;
-  int xcu = x,ycu = y;
-  int check = 0;
-  while(true)
-    {
-      gotoXY(xcu,ycu);
-      cout<<"                     ";
-      gotoXY(10,0);
-      cout << "HOC LAP TRINH THAT DE";
-      xcu = x;
-      ycu = y;
-      SetColor(i);
-      i++;
-      if(i>15){
-        i=1;
-      }
-      //dieu khien
-      if(_kbhit())
-      {
-        char kitu = _getch();
-        if(kitu == -32)
-        {
-          kitu = _getch();
-          if( kitu == 72)//di len
-          {
-            check = 1;
-          }
-          else if(kitu == 80)//di xuong
-          {
-            check = 0;
-          }
-          else if(kitu == 75)
-          {
-            check =2;
-          }
-          else if(kitu == 77)
-          {
-            check = 3;j
-          }
+void SetColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+// ==========================================
+
+int main() {
+    int i = 1;
+    int x = 10, y = 0;
+    int xcu = x, ycu = y;
+    int check = 0;
+
+    while (true) {
+        gotoXY(xcu, ycu);
+        cout << "                     ";
+
+        gotoXY(10, 0);
+        cout << "HOC LAP TRINH THAT DE";
+
+        xcu = x;
+        ycu = y;
+
+        SetColor(i);
+        i++;
+        if (i > 15) i = 1;
+
+        // Ði?u khi?n phím mui tên
+        if (_kbhit()) {
+            char kitu = _getch();
+            if (kitu == -32) {
+                kitu = _getch();
+                if (kitu == 72) check = 1; // lên
+                else if (kitu == 80) check = 0; // xu?ng
+                else if (kitu == 75) check = 2; // trái
+                else if (kitu == 77) check = 3; // ph?i
+            }
         }
-      }
-      //kiem tra huong
-      if(check == 0)
-      {
-        y++;
-      }
-      else if(check == 1)
-      {
-        y--;
-      }
-      else if(check == 2)
-      {
-        x--;
-      }
-      else if (check == 3)
-      {
-        x++;
-      }
-      //kiem tra bien
-      if(y == 28)
-      {
-        check = 1;
-      }
-      else if (y == 0)
-      {
-        check = 0;
-      }
-      else if (x == 0)
-      {
-        check = 3;
-      }
-      else if (x == 100)
-      {
-        check = 2;
-      }
-      Sleep(100);
+
+        // Di chuy?n
+        if (check == 0) y++;
+        else if (check == 1) y--;
+        else if (check == 2) x--;
+        else if (check == 3) x++;
+
+        // Gi?i h?n biên
+        if (y == 28) check = 1;
+        else if (y == 0) check = 0;
+        else if (x == 0) check = 3;
+        else if (x == 100) check = 2;
+
+        Sleep(100);
     }
-  _getch();
-  return 0;
+
+    _getch();
+    return 0;
 }
