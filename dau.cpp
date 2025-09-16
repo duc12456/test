c05243b6efd122c7a4cd580adbfbf06721889649
duc12456 <andanh1234567888@gmail.com>
2025-09-16 15:37:52 +0700

    Update dau.cpp

--- dau.cpp
+++ dau.cpp
@@ -1,36 +1,21 @@
-#include <bits/stdc++.h>
-#include <windows.h>
 #include <conio.h>
-
+#include<iostream>
+#include <windows.h>
+#include "mylib.h"
 using namespace std;
 
-void gotoXY(int x, int y) 
-{
-    HANDLE hConsoleOutput;
-    COORD Cursor_an_Pos = { (SHORT)x, (SHORT)y };
-    hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleCursorPosition(hConsoleOutput, Cursor_an_Pos);
-}
-
-void SetColor(int color) 
-{
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-}
-
-int main() 
-{
+int main() {
     int i = 1;
     int x = 10, y = 0;
     int xcu = x, ycu = y;
     int check = 0;
 
-    while (true) 
-    {
+    while (true) {
         gotoXY(xcu, ycu);
         cout << "                     ";
 
-        gotoXY(10, 0);
-        cout << "Snake game";
+        gotoXY(x, y);
+        cout << "HOC LAP TRINH THAT DE";
 
         xcu = x;
         ycu = y;
@@ -39,24 +24,26 @@ int main()
         i++;
         if (i > 15) i = 1;
 
-        if (_kbhit())  // Dieu khien theo mui ten
-        {
+        // Ðieu khien phím mui tên
+        if (_kbhit()) {
             char kitu = _getch();
             if (kitu == -32) {
                 kitu = _getch();
-                if (kitu == 72) check = 1; // len
-                else if (kitu == 80) check = 0; // xuong
-                else if (kitu == 75) check = 2; // trai
-                else if (kitu == 77) check = 3; // phai
+                if (kitu == 72) check = 1; // lên
+                else if (kitu == 80) check = 0; // xu?ng
+                else if (kitu == 75) check = 2; // trái
+                else if (kitu == 77) check = 3; // ph?i
             }
         }
 
-        if (check == 0) y++; // di chuyen con ran
+        // Di chuy?n
+        if (check == 0) y++;
         else if (check == 1) y--;
         else if (check == 2) x--;
         else if (check == 3) x++;
-        
-        if (y == 28) check = 1; // gioi han bien cua 
+
+        // Gi?i h?n biên
+        if (y == 28) check = 1;
         else if (y == 0) check = 0;
         else if (x == 0) check = 3;
         else if (x == 100) check = 2;
